df091d909d6e71b7bd1714ed956ae5e7f628fbd6
Eric Mesa <ericmesa2014@gmail.com>
2022-11-17 22:41:54 -0300

    algo

--- js/login.js
+++ js/login.js
@@ -28,7 +28,6 @@ document.addEventListener("DOMContentLoaded", function () {
           event.stopPropagation();
         } else {
           localStorage.setItem("nombre", user);
-          location.href = "index.html";
         }
         form.classList.add("was-validated");
       },
